7b59f181ed28e938341036933ebaaf3207194c49
Kymeii99 <kimthai0818@gmail.com>
2023-09-25 19:52:03 +0100

    Adding in green box with student no. and name

---
+++ Lab1-C19358373/script.js
@@ -0,0 +1,22 @@
+alert('Hello World') 
+
+//Getting the text
+var textFade = document.querySelector('.heading');
+textFade.innerHTML = textFade.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+//animating the text
+anime.timeline({loop: true})
+  .add({
+    targets: '.heading .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.heading',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
